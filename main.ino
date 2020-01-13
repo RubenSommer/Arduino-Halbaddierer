4cd7af5e85778fa64ff901ec1c7e212a9350f15a
ProfiBrunoHacker <ruben.sommer@t-online.de>
2020-01-13 19:33:48 +0100

    update

--- main.ino
+++ main.ino
@@ -1,13 +1,26 @@
 void setup() {
+
     /*set pinmode of GPIO pins */
     pinMode(2, OUTPUT);
     pinMode(3, OUTPUT);
     pinMode(4, INPUT);
     pinMode(5, INPUT);
-    /* set baud rate*/
+
+    /*set baud rate*/
     Serial.begin(9600);
+
+    /*set integers*/
+    byte input1;
+    byte input2;
+    byte output1;
+    byte output2;
 }
 
-void loop() {
+void loop () {
+    Serial.println("Please enter your numbers");    /*Client can interact*/
+
+    byte input1 = Serial.read();
+    byte input2 = Serial.read();
     
 }
+
